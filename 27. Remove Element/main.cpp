a8a489392a69ccb59e610c18498a5044157dbde2
陈宇峰 <yufengcode@gmail.com>
2016-10-04 15:40:50 +0800

    init

--- 27. Remove Element/main.cpp
+++ 27. Remove Element/main.cpp
@@ -7,9 +7,57 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int removeElement(vector<int>& nums, int val)
+{
+    sort(nums.begin(), nums.end());
+    long length = nums.size();
+
+    long startIndex = 0, endIndex = nums.size() - 1;
+    bool isStartFind = false,isEndFind = false;
+    for (long i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] == val)
+        {
+            startIndex = i;
+            isStartFind = true;
+            break;
+        }
+    }
+    for (long i = nums.size() - 1; i >= 0; i--)
+    {
+        if (nums[i] == val)
+        {
+            endIndex = i;
+            isEndFind = true;
+            break;
+        }
+    }
+
+    if (isStartFind && isEndFind)
+    {
+        return (int)(length - (endIndex - startIndex + 1));
+    }
+    else
+    {
+        return (int)length;
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    int arr[] = {4,5};
+    vector<int>vv(arr,arr + sizeof(arr) / sizeof(int));
+    int result = removeElement(vv, 4);
+    cout << result;
+
     return 0;
 }
+
+
+
+
+
